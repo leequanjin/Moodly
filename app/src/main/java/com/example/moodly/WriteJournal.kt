cb824b5c3c7300dbc281403531c743c3b0ea5f81
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2024-03-15 14:08:07 +0800

    updated write journal activity design

--- app/src/main/java/com/example/moodly/WriteJournal.kt
+++ app/src/main/java/com/example/moodly/WriteJournal.kt
@@ -1,6 +1,11 @@
 package com.example.moodly
 
+import android.app.Dialog
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moodly.databinding.ActivityWriteJournalBinding
 
@@ -11,5 +16,20 @@ class WriteJournal : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteJournalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnEnhance.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottomsheet_layout)
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
+        dialog.window?.setDimAmount(0.1f)
     }
 }
