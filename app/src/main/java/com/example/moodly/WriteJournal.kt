4aa8c3b339779d3d06fd12fa251c8c27e8faef75
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2024-03-15 15:28:26 +0800

    connected to firebase

--- app/src/main/java/com/example/moodly/WriteJournal.kt
+++ app/src/main/java/com/example/moodly/WriteJournal.kt
@@ -8,18 +8,43 @@ import android.view.ViewGroup
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moodly.databinding.ActivityWriteJournalBinding
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 
 class WriteJournal : AppCompatActivity() {
 
     private lateinit var binding: ActivityWriteJournalBinding
+    private lateinit var database: DatabaseReference
+
+    data class JournalEntry(
+        val content: String,
+        val date: String
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteJournalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // connect to firebase
+        database = Firebase.database.reference
+
+        binding.btnSave.setOnClickListener {
+            val date = binding.tvDate.toString()
+            val content = binding.etContent.toString()
+
+            database.child("DiaryRecords").child("Mar2024").child("Content").setValue(content)
+            database.child("DiaryRecords").child("Mar2024").child("Content").setValue(date)
+        }
+
+        // enhance confirmation dialog
         binding.btnEnhance.setOnClickListener {
             showDialog()
         }
     }
+
+
 
     private fun showDialog() {
         val dialog = Dialog(this)
