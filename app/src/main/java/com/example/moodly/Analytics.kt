13b4a42f7d9b4f1a2236ecf45ae7f9e5d48bf187
Eddead <simhk-wp22@student.tarc.edu.my>
2024-03-20 02:46:39 +0800

    Update to analytic

--- app/src/main/java/com/example/moodly/Analytics.kt
+++ app/src/main/java/com/example/moodly/Analytics.kt
@@ -2,6 +2,7 @@ package com.example.moodly
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +13,11 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +33,9 @@ class Analytics : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private lateinit var SLD: SaveLoadData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +51,8 @@ class Analytics : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_analytics, container, false)
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -52,6 +63,46 @@ class Analytics : Fragment() {
     }
 
     private fun setupPieChart() {
+
+        auth = com.google.firebase.Firebase.auth
+        database = Firebase.database.reference
+        SLD = SaveLoadData()
+
+        val id = auth.currentUser?.uid.toString()
+
+        var awesomeCount = 0
+        var goodCount = 0
+        var mehCount = 0
+        var downCount = 0
+        var terribleCount = 0
+        var moodlessCount = 0
+
+        database.child(id).child("JournalEntries").get()
+            .addOnSuccessListener {
+                Log.d("Moodly", "What's the value: ${it.value}")
+                for (year in it.children){
+                    for (month in year.children){
+                        for (day in month.children) {
+                            val mood = day.child("mood").value
+                            Log.d("Moodly", "What's the mood: $mood")
+                            if (mood == "Feeling Awesome!") {
+                                awesomeCount++
+                            } else if (mood == "Feeling Good"){
+                                goodCount++
+                            } else if (mood == "Feeling Meh"){
+                                mehCount++
+                            } else if (mood == "Feeling Down"){
+                                downCount++
+                            } else if (mood == "Feeling Terrible..."){
+                                terribleCount++
+                            }else{
+                                moodlessCount++
+                            }
+                        }
+                    }
+                }
+            }
+
         // Get reference to the PieChart
         val pieChart: PieChart = view?.findViewById(R.id.pieChart) ?: return
 
@@ -59,17 +110,32 @@ class Analytics : Fragment() {
         val entries:ArrayList<PieEntry> = ArrayList()
 
         //Value is how big the area taken, Label is name ("n"f for number and "..." for string)
-        entries.add(PieEntry(30f, "Happy"))
-        entries.add(PieEntry(20f, "Angry"))
-        entries.add(PieEntry(50f, "Sad"))
-        entries.add(PieEntry(50f, "Normal"))
+        entries.add(PieEntry((awesomeCount)+0f, "Feeling \nAwesome!"))
+        entries.add(PieEntry((goodCount)+0f, "Feeling \nGood"))
+        entries.add(PieEntry((mehCount)+0f, "Feeling \nMeh"))
+        entries.add(PieEntry((downCount)+0f, "Feeling \nDown"))
+        entries.add(PieEntry((terribleCount)+0f, "Feeling \nTerrible..."))
+        entries.add(PieEntry((moodlessCount)+0f, "Moodless"))
+
+
 
         val pieDataSet= PieDataSet(entries  , "")
 
-        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS, 255)
+        // Colors representing the rainbow spectrum
+        val colors = intArrayOf(
+            Color.parseColor("#FFC0CB"), // Pink
+            Color.RED,
+            Color.parseColor("#FF7F00"), // Orange
+            Color.BLUE,
+            Color.parseColor("#7F00FF"), // Indigo
+            Color.WHITE // Violet
+        )
+
+        pieDataSet.setColors(colors, 255)
 
         pieDataSet.valueTextSize=12f
         pieDataSet.valueTextColor= Color.BLACK
+
 
         // Set the size of the center hole (inner circle)
         val holeRadius = 35f // Adjust the value to make the hole smaller
@@ -78,8 +144,9 @@ class Analytics : Fragment() {
         // Set the size of the transparent circle around the hole
         pieChart.transparentCircleRadius = holeRadius
 
-        pieChart.setEntryLabelTextSize(15f)
-        pieChart.setCenterTextColor(Color.BLUE)
+        pieChart.setEntryLabelTextSize(13f)
+        pieChart.setEntryLabelColor(Color.BLACK)
+        pieChart.setCenterTextColor(Color.BLACK)
 
         val pieData= PieData(pieDataSet)
 
@@ -89,7 +156,7 @@ class Analytics : Fragment() {
 
         pieChart.description.text="Pie Chart"
         pieChart.centerText= "Mood \nTracker"
-        pieChart.setCenterTextSize(14f)
+        pieChart.setCenterTextSize(16f)
         //pieChart.setCenterTextColor(Color.BLUE);
 
         pieChart.animateY(2000)
