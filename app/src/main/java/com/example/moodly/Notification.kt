498255fdc817aeee63342b215bc8d69cdd2dda36
Eddead <simhk-wp22@student.tarc.edu.my>
2024-03-20 15:45:27 +0800

    Allow Notification

--- app/src/main/java/com/example/moodly/Notification.kt
+++ app/src/main/java/com/example/moodly/Notification.kt
@@ -1,6 +1,7 @@
 package com.example.moodly
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -11,10 +12,14 @@ const val channelID = "Channel1"
 
 class Notification : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent){
+        val notificationIntent = Intent(context, MainActivity::class.java) // Replace with your app's main activity
+        notificationIntent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK) // Only use this flag
+        val pendingIntent = PendingIntent.getActivity(context, notificationID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
         val notification = NotificationCompat.Builder(context, channelID)
             .setSmallIcon(R.drawable.ic_journal)
             .setContentTitle("Moodly Reminder") // Set title directly
             .setContentText("It's time to reflect on your mood!") // Set message directly
+            .setContentIntent(pendingIntent) // Set the pending intent here
             .build()
 
         val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
